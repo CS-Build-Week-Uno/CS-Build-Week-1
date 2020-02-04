b3e29cb6142729afe80a40b4ce2b739f9b7174ac
Derek Etman <dereketman@gmail.com>
2020-02-03 21:20:54 -0800

    Added Notes

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -20,16 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+ALLOWED_HOSTS = []
+
+# Application definition
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
