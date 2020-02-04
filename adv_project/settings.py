224195dcbe7735ddc36d650b68fccb984d9b6166
Jeff Butler <jeff.butler.springdale@gmail.com>
2020-02-04 12:25:14 -0600

    made models

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,9 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Application definition
 
@@ -151,3 +155,6 @@ STATIC_URL = '/static/'
 
 import django_heroku
 django_heroku.settings(locals())
+
+if(config('DEBUG', cast=bool) == True):
+    del DATABASES['default']['OPTIONS']['sslmode']
