4a4947501d32721cd4334ead21d98dc0e40cb11f
Jeff Butler <jeff.butler.springdale@gmail.com>
2020-02-07 00:52:02 -0600

    push to master and try to not make this die on deploy

--- util/generator.py
+++ util/generator.py
@@ -1,5 +1,6 @@
 from adventure.models import Room, Player
-
+import random
+Room.objects.all().delete()
 
 class Map:
     def __init__(self):
@@ -45,8 +46,18 @@ class Map:
                 direction *= -1
 
             # Create a room in the given direction
-            room = Room(room_count, "A Generic Room",
-                        "This is a generic room.", x, y)
+
+            roomName=["No Death", "Not Allowed to die here", "Its ok to die here", "Death of Death", "9 Lives", "1 Life", "A lot of Death", "Life?(Nope just Death)", "Death's Door", "To Die or Not to Die", "Nuke Town"]
+
+            roomDescription=["There shall never be a death in this room", "Physically not allowed to die here, if the rule is broken, the punishment is death", "It's completely ok to die here :)", "Death", "A cat lives in this room, but still has all of its lives", "A scraggly cat lives here, he's on his last life", "“Literally everything is dead", "There is a green plant in front of you that looks alive and healthy, but it's really just plastic and super dead", "There is a door on the roof with satan", "You are given a choice, to read a MLP fanfiction, or die", "You take a 360 no scope to the head, but it's just a hit marker and the enemy is raging out of his mind"]
+
+            randomRoomName = random.choice(roomName)
+            randomRoomDescription = random.choice(roomDescription)
+
+            room = Room(room_count, randomRoomName,
+                        randomRoomDescription, x, y)
+            # room = Room(room_count, "A Generic Room",
+            #             "This is a generic room.", x, y)
             # Note that in Django, you'll need to save the room after you create it
             room.save()
 
